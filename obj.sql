d78937f06534f3d11849882fcb001b307f0a24ed
m r <trash89@laposte.net>
2022-11-29 07:33:04 +0100

    upd

--- obj.sql
+++ obj.sql
@@ -1,14 +1,14 @@
 set lines 200 pages 22 trims off trim on
-undef own
-accept own char prompt 'Owner?(%)      :' default '%'
+
 col obj format a39 head 'Object Name'
 col object_type for a22 head 'Object Type'
 col object_id for 9999999 head 'Obj ID'
 col temporary for a4 head 'Temp'
 col generated for a3 head 'Gen'
 col secondary for a3 head 'Sec'
+
 select 
-   owner||'.'||object_name as obj,
+   object_name as obj,
    object_type,
    created,
    last_ddl_time,
@@ -17,58 +17,13 @@ select
    temporary,
    generated,
    secondary 
-from dba_objects
-where 
-     owner like upper('%&&own%') and owner not in (
-                   'SYS',
-                   'SYSTEM',
-                   'RECOVERY_CATALOG_OWNER',
-		   'EXECUTE_CATALOG_ROLE',
-		   'SELECT_CATALOG_ROLE',
-                   'DBA',
-                   'RESOURCE',
-                   'CONNECT',
-                   'SNMPAGENT',
-                   'IMP_FULL_DATABASE',
-                   'EXP_FULL_DATABASE',
-                   'PERFSTAT',
-                   'OUTLN',
-                   'DBSNMP',
-		   'AQ_USER_ROLE',
-		   'HS_ADMIN_ROLE',
-		   'WM_ADMIN_ROLE',
-		   'CTXAPP',
-		   'LBAC_DBA',
-                   'AQ_ADMINISTRATOR_ROLE',
-		   'AURORA$JIS$UTILITY$',
-		   'AURORA$ORB$UNAUTHENTICATED',
-		   'BC4J',
-		   'CTXSYS',
-		   'JAVADEBUGPRIV',
-		   'JAVASYSPRIV',
-		   'LBACSYS',
-		   'MDSYS',
-		   'OEM_MONITOR',
-		   'OLAPDBA',
-		   'OLAPSVR',
-		   'OLAPSYS',
-		   'OLAP_DBA',
-		   'ORDPLUGINS',
-		   'ORDSYS',
-		   'OSE$HTTP$ADMIN',
-		   'RECOVERY_CATALOG_OWNER',
-		   'TIMESERIES_DBA',
-		   'TIMESERIES_DEVELOPER',
-		   'TRACESVR',
-		   'WKSYS',
-		   'WKUSER',
-		   'WKADMIN',
-                   'PTRLADM',
-		   'PUBLIC')
+from user_objects
+where object_type not like '%PARTITION%'
 order by 
-  owner,
   object_type,
   object_name;
-undef own
+
+select object_type,count(0) from user_objects group by object_type order by 1;
+
 clear col
 set lines 80 pages 22 feed on head on
--- tab.sql
+++ tab.sql
@@ -1,75 +1,38 @@
 @save_sqlplus_settings
 
 set lines 200 pages 22 trims off trim on
-undef own
 undef tab
 undef tbs
 accept tbs char prompt 'Tablespace?(%) :' default ''
-accept own char prompt 'Owner?(%)      :' default ''
 accept tab char prompt 'Table?(%)      :' default ''
 column tab format a39 head 'Table'
-column pct_free_used format a6 head '%f/us'
+column pct_free_used format a6 head '%f/%us'
 column ini_max format a10 head 'Ini/Max/Fl'
 column num_rows format 99999999 head 'NrRows'
-column blo format a13 head 'Used/EmptyBlk'
-column degrI format a7 head 'Degr'
-column part format a24 head 'Ch/Part/Tmp/Nes/RowM/Mon'
+column blo format a13 head 'Blk/Empty'
+column degrI format a7 head 'Paral'
+column part format a30 head 'Part/Cached/Tmp/Nes/RowM/Mon'
 column splen format a10 head 'AvgSp/Row'
 column chain_cnt format 99999 head 'ChCnt'
 column avg_row_len format 999999 head 'AvgRow'
 column lasta format a10 head 'LastAnl'
 column ininext format a10 head 'KIni/Next'
 select 
-   owner||'.'||table_name as tab, 
+   table_name as tab, 
    to_char(pct_free)||'/'||to_char(pct_used) as pct_free_used,
    to_char(initial_extent/1024)||'/'||to_char(next_extent/1024) as ininext,
    to_char(ini_trans)||'/'||to_char(max_trans)||'/'||to_char(freelists) as ini_max,
    num_rows,
+   to_char(last_analyzed,'dd/mm/rrrr') as lasta,   
    to_char(blocks)||'/'||to_char(empty_blocks) as blo,
    to_char(avg_space)||'/'||to_char(avg_row_len) as splen,
    chain_cnt,
---   avg_space_freelist_blocks,
---   num_freelist_blocks,
-   substr(trim(degree),1,3)||'/'||substr(trim(instances),1,3) as degrI,
-   to_char(last_analyzed,'dd/mm/rrrr') as lasta,
-   cache||'/'||partitioned||'/'||temporary||'/'||nested||'/'||substr(row_movement,1,3)||'/'||monitoring as part
-from dba_all_tables 
+   trim(degree)||'/'||trim(instances) as degrI,
+   trim(partitioned)||'/'||trim(cache)||'/'||trim(temporary)||'/'||trim(nested)||'/'||trim(row_movement)||'/'||trim(monitoring) as part
+from user_tables 
 where 
-     owner like upper('%&&own%') and table_name like upper('%&&tab%') and (tablespace_name like upper('%&&tbs%') or tablespace_name is null)
-     and owner not in 
-         ('SYS',
-                      'SYSTEM',
-                      'OUTLN',
-                      'DBSNMP',
-                      'CTXSYS',
-                      'DRSYS',
-                      'MDSYS',
-                      'ORDSYS',
-                      'ORDPLUGINS',
-                      'TRACESVR',
-                      'AURORA$JIS$UTILITY$',
-                      'AURORA$ORB$UNAUTHENTICATED',
-                      'LBACSYS',
-                      'OLAPDBA',
-                      'OLAPSVR',
-                      'OLAPSYS',
-                      'OSE$HTTP$ADMIN',
-                      'WKSYS',
-      'XDB',
-      'WMSYS',
-      'WK_TEST',
-      'WK_PROXY',
-      'SYSMAN',
-      'SI_INFORMTN_SCHEMA',
-      'SCOTT',
-      'MGMT_VIEW',
-      'MDDATA',
-      'EXFSYS',
-      'DMSYS',
-      'ANONYMOUS',
-      'BC4J') 
-order by owner,num_rows ;
-undef own
+     table_name like upper('%&&tab%') and (tablespace_name like upper('%&&tbs%') or tablespace_name is null)
+order by num_rows,table_name ;
 undef tab
 undef tbs
 
